8733970c5307441aad52e2ab2688e868ef09d1b5
Luc Bourlier <lb@skyluc.org>
2012-03-12 17:11:21 +0100

    Fixed scala thread tests. Prerequisite had changed.

--- org.scala-ide.sdt.debug.tests/src/scala/tools/eclipse/debug/model/ScalaThreadTest.scala
+++ org.scala-ide.sdt.debug.tests/src/scala/tools/eclipse/debug/model/ScalaThreadTest.scala
@@ -8,49 +8,65 @@ import com.sun.jdi.VMDisconnectedException
 import org.junit.Before
 import org.eclipse.debug.core.DebugPlugin
 import com.sun.jdi.ObjectCollectedException
+import com.sun.jdi.ThreadGroupReference
+
+object ScalaThreadTest {
+  private def createThreadGroup() = {
+    val jdiThreadGroup = mock(classOf[ThreadGroupReference])
+    when(jdiThreadGroup.name).thenReturn("some")
+    jdiThreadGroup;
+  }
+}
 
 /**
  * Tests for ScalaThread.
  */
 class ScalaThreadTest {
-  
+  import ScalaThreadTest._
+
   @Before
   def initializeDebugPlugin() {
     if (DebugPlugin.getDefault == null) {
       new DebugPlugin
     }
   }
-  
+
   @Test
   def getName() {
-    val jdiThread= mock(classOf[ThreadReference])
-    
+    val jdiThread = mock(classOf[ThreadReference])
+
     when(jdiThread.name).thenReturn("some test string")
-    
-    val thread= new ScalaThread(null, jdiThread)
-    
+    val jdiThreadGroup = createThreadGroup();
+    when(jdiThread.threadGroup).thenReturn(jdiThreadGroup)
+
+    val thread = new ScalaThread(null, jdiThread)
+
     assertEquals("Bad thread name", "some test string", thread.getName)
   }
-  
+
   @Test
   def vmDisconnectedExceptionOnGetName() {
-    val jdiThread= mock(classOf[ThreadReference])
-    
+    val jdiThread = mock(classOf[ThreadReference])
+
     when(jdiThread.name).thenThrow(new VMDisconnectedException)
-    
-    val thread= new ScalaThread(null, jdiThread)
-    
+    val jdiThreadGroup = createThreadGroup();
+    when(jdiThread.threadGroup).thenReturn(jdiThreadGroup)
+
+    val thread = new ScalaThread(null, jdiThread)
+
     assertEquals("Bad thread name on VMDisconnectedException", "<disconnected>", thread.getName)
   }
-  
+
   @Test
   def objectCollectedExceptionOnGetName() {
-    val jdiThread= mock(classOf[ThreadReference])
-    
+    val jdiThread = mock(classOf[ThreadReference])
+
     when(jdiThread.name).thenThrow(new ObjectCollectedException)
-    
-    val thread= new ScalaThread(null, jdiThread)
-    
+    val jdiThreadGroup = createThreadGroup();
+    when(jdiThread.threadGroup).thenReturn(jdiThreadGroup)
+
+    val thread = new ScalaThread(null, jdiThread)
+
     assertEquals("Bad thread name", "<garbage collected>", thread.getName)
   }
 
