e159e53acc228539194649472a748dac5b44de9e
Luc Bourlier <lb@skyluc.org>
2012-03-29 15:39:57 +0200

    Added check to avoid abstract methods in anon func search

--- org.scala-ide.sdt.debug/src/scala/tools/eclipse/debug/command/ScalaStepOver.scala
+++ org.scala-ide.sdt.debug/src/scala/tools/eclipse/debug/command/ScalaStepOver.scala
@@ -76,7 +76,7 @@ object ScalaStepOver {
   def anonFunctionsInRange(refType: ReferenceType, range: Range) = {
     import scala.collection.JavaConverters._
     val methods = refType.methods.asScala.filter(method =>
-      range.contains(method.location.lineNumber) && method.name.startsWith("apply"))
+       method.name.startsWith("apply") && !method.isAbstract && range.contains(method.location.lineNumber))
 
     // TODO: using isBridge was not working with List[Int]. Should check if we can use it by default with some extra checks when it fails.
     //      methods.find(!_.isBridge)
